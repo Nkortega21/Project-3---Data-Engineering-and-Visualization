c39e728b2f59c89b3a9a20c5706db0ca0bb0a5a9
Wei Wang <weiwang12060725@gmail.com>
2024-10-21 21:36:05 -0700

    Update project3_sql_schema.sql

--- SQL_Info/project3_sql_schema.sql
+++ SQL_Info/project3_sql_schema.sql
@@ -1,20 +1,20 @@
 -- Create Orders table
 CREATE TABLE Orders (
     OrderID VARCHAR(50) PRIMARY KEY,
-    Date DATE NULL, 
-    Status VARCHAR(50) NULL,
-    Fulfilment VARCHAR(50) NULL,
-    SalesChannel VARCHAR(50) NULL,
-    ShipServiceLevel VARCHAR(50) NULL,
+    Date DATE NOT NULL, 
+    Status VARCHAR(50) NOT NULL,
+    Fulfilment VARCHAR(50) NOT NULL,
+    SalesChannel VARCHAR(50) NOT NULL,
+    ShipServiceLevel VARCHAR(50) NOT NULL,
     B2B BOOLEAN NOT NULL
 );
 
 -- Create ShippingDetails table
 CREATE TABLE ShippingDetails (
     OrderID VARCHAR(50) PRIMARY KEY,
-    ShipCity VARCHAR(100) NULL, 
+    ShipCity VARCHAR(100)  NULL, 
     ShipState VARCHAR(100) NULL,  
-    ShipPostalCode VARCHAR(20) NULL,  
+    ShipPostalCode VARCHAR(20) NOT NULL,  
     ShipCountry VARCHAR(50) NULL,  
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
@@ -23,11 +23,11 @@ CREATE TABLE ShippingDetails (
 CREATE TABLE Products (
     SKU VARCHAR(50),
     OrderID VARCHAR(50),
-    Style VARCHAR(50) NULL, 
-    Category VARCHAR(50) NULL, 
-    Qty INT NULL, 
-    Currency VARCHAR(10) NULL,  
-    Amount DECIMAL(10, 2) NULL,  
+    Style VARCHAR(50) NOT NULL, 
+    Category VARCHAR(50) NOT NULL, 
+    Qty INT NOT NULL, 
+    Currency VARCHAR(10) NOT NULL,  
+    Amount DECIMAL(10, 2) NOT NULL,  
     PRIMARY KEY (OrderID, SKU),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
@@ -37,6 +37,5 @@ CREATE TABLE completed_orders (
     OrderID VARCHAR(50) PRIMARY KEY,  
     Status VARCHAR(100) NOT NULL,      
     Fulfilment VARCHAR(100),            
-    Amount DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+    Amount DECIMAL(10, 2) NOT NULL      
 );
